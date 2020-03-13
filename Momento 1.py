32d1f87d79d57e910ce2663c302462c3b5cfd593
parralopera <54422368+parralopera@users.noreply.github.com>
2020-03-13 10:08:59 -0500

    Add files via upload

---
+++ Momento 1.py
@@ -0,0 +1,52 @@
+# Momento 1
+
+programa = sistemas and finanzas
+alumno_count = 0
+count_sexM = 0
+count_sexF = 0
+total = 0.0
+
+while true:
+    alumno = int(input("ingrese nombre de Alumno: "))
+    average = float(input("Ingrese notas: "))
+    alumno = alumno_count+1
+    nota = notas + nota
+
+for i in range (alumno):
+print "Escriba la nota de las 5 clases"
+
+nota1 = float(input('Nota1: '))
+nota2 = float(input('Nota2: '))
+nota3 = float(input('Nota3: '))
+nota4 = float(input('Nota4: '))
+nota5 = float(input('Nota5: '))
+
+total=nota1+nota2+nota3+nota4+nota5
+total=total/5
+
+# Parte II
+
+gender=input("Sexo del alumno: ")
+    count_sexM == "m"
+    count_sexF == "f"
+
+    for i in range (alumno):
+        gender = input("Ingrese genero del alumno: ")
+
+        if gender == "m":
+            count_sexM+=1
+        else gender =="f":
+            count_sexF+=1
+    for i in range (alumno):
+
+        promedad = input("Ingrese edad del alumno: ")
+
+    print "Cantidad de alumnos matriculados es: " ,alumno_count
+    print "Promedio de edad " ,promedad
+    print "El total de sus 5 calificaciones es: ",total
+
+
+close_program= input ("Desea continuar: Si-No")
+
+if close_program != 'Si':
+    break
